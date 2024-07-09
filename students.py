d84d03bfdb43f501b297a3e6156532831b37b3f5
HristoYan <hristoyan@abv.bg>
2024-07-09 20:30:35 +0300

    implement add_grade, calculate_average, add_student

--- students.py
+++ students.py
@@ -2,16 +2,26 @@ students = {}
 
 
 def add_student(name):
-    pass
+    name = input("Enter Student Name: ")
+    students[name] = []
 
 
 def add_grade(name, grade):
-    pass
+    name = input("Enter student\'s name: ")
+    grades = input("Enter grades separated by comma: ")
+    grades_list = grades.join(', ')
+    students[name] = grades_list
 
 
 def calculate_average(name):
-    pass
+    name = input("Enter student\'s name: ")
+    grades = students[name]
+    sum_grade = 0
+    for grade in grades:
+        sum_grade += grade
 
+    avr_grade = sum_grade / len(grades)
+    print(f"The average grade of student {name} is: {avr_grade}")
 
 def display_students():
     pass
