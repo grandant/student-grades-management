b086f87fb95d5bbc44c34774edfc63ea8625bf53
Peter Bozukov <bozukov.p@gmail.com>
2024-07-09 18:04:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import students
+
+
+def main():
+    while True:
+        print("\nStudent Grades Management System")
+        print("1. Add a new student")
+        print("2. Add grades for a student")
+        print("3. Calculate the average grade for a student")
+        print("4. Display all students and their grades")
+        print("5. Remove a student")
+        print("6. Update a student's grade")
+        print("7. Get a student's highest grade")
+        print("8. Save student data to a file")
+        print("9. Load student data from a file")
+        print("10. Exit")
+
+        break
---
+++ students.py
@@ -0,0 +1,37 @@
+students = {}
+
+
+def add_student(name):
+    pass
+
+
+def add_grade(name, grade):
+    pass
+
+
+def calculate_average(name):
+    pass
+
+
+def display_students():
+    pass
+
+
+def remove_student(name):
+    pass
+
+
+def update_grade(name, old_grade, new_grade):
+    pass
+
+
+def get_highest_grade(name):
+    pass
+
+
+def save_data(filename='data/students_data.txt'):
+    pass
+
+
+def load_data(filename='data/students_data.txt'):
+    pass
